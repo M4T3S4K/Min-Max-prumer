dca6a3a818dc5c0e4950aea3d6fc09b7a90831d5
matej.vokal <matej.vokal@W10A212.sps-cl.local>
2018-04-09 15:31:38 +0200

    first commit

---
+++ src/min/max/prumer/MinMaxPrumer.java
@@ -0,0 +1,29 @@
+
+package min.max.prumer;
+import java.util.Scanner;
+
+public class MinMaxPrumer {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner (System.in, "CP1250");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int soucet = 0, pocet = 1;
+        // nekonečná smyčka
+    while (true){
+        pocet++;
+        System.out.print("vlož"+pocet+ ".číslo: ");
+        int vstup = sc.nextInt();
+        //pokud vložil menší nebo větší číslo, zapamatujeme si je
+        // pokud uživatel vloží nulu program skončí
+        if (vstup == 0) break;
+        soucet = soucet + vstup;
+        if (vstup < min) min = vstup;
+        if(vstup > max) max = vstup;
+        System.out.println("minimum: "+min);
+        System.out.println("maximum: "+max);
+        System.out.println("průměr: "+((float) soucet/pocet));
+        }
+    }
+}
+
